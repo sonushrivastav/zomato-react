e26e82172d6661d67b4530fc048571d7a05d217b
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-19 21:27:35 +0530

    adding footer file with css

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./home.css"
 import Header from '../../components/header/Header'
 import TabSections from '../../components/tabsection/TabSections'
+import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   return (
     <div>
       <Header />
-      <TabSections/>
+      <TabSections />
+      <Footer/>
     </div>
   )
 }
