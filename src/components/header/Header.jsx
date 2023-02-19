2f91a77a2a5df275281ee68eb32638babd4d8791
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-19 22:22:23 +0530

    added zomato image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import "./header.css"
 const Header = () => {
   return (
-    <div className='max_width'>
-      <h1>this is header</h1>
+    <div className='max_width header'>
+     <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="Zomato-logo" className='header_logo' />
     </div>
   )
 }
