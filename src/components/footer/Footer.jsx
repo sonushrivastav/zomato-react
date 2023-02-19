3c67155894b5d992d48551ec0d4c298d0d9addf9
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-19 21:35:44 +0530

    adding logic for tab section switch case

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.css"
 const Footer = () => {
   return (
     <div>
-      
+      <h1>this is footer</h1>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./home.css"
 import Header from '../../components/header/Header'
 import TabSections from '../../components/tabsection/TabSections'
 import Footer from '../../components/footer/Footer'
 
 const Home = () => {
+  const [activeTab,setActiveTab]=useState("Nightlife")
+
+  const getCorrectScreen = (tab) => {
+    switch (tab) {
+      case "Delivery":
+        return <div>Delivery</div>;
+      case "Dinning Out":
+        return <div>Dinning Out</div>;
+      case "Nightlife":
+        return <div>Nightlife</div>;
+      default:
+        return <div>Delivery</div>
+    }
+  }
   return (
     <div>
       <Header />
       <TabSections />
+      {getCorrectScreen(activeTab)}
       <Footer/>
     </div>
   )
