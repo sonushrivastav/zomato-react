53f8442badf44079e34971e2694b7cfed206bbc9
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-19 21:19:05 +0530

    adding header footer tabsection file

--- src/components/tabsection/TabSections.jsx
+++ src/components/tabsection/TabSections.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./tabsections.css"
 const TabSections = () => {
   return (
     <div>
-      
+      <h1>this is tabsection</h1>
     </div>
   )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import "./home.css"
 import Header from '../../components/header/Header'
+import TabSections from '../../components/tabsection/TabSections'
 
 const Home = () => {
   return (
     <div>
-      <Header/>
+      <Header />
+      <TabSections/>
     </div>
   )
 }
