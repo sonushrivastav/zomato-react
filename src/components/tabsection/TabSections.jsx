1d3305b23830b7a2c00b5c329ca23d4113465577
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-19 22:11:57 +0530

    making common css classes for app

--- src/components/tabsection/TabSections.jsx
+++ src/components/tabsection/TabSections.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./tabsections.css"
-const TabSections = () => {
+const TabSections = ({activetab,setActiveTab}) => {
   return (
     <div>
-      <h1>this is tabsection</h1>
+      <h1>this is tab option</h1>
     </div>
   )
 }
