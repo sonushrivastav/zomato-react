2193012f93c519eeadfdc0882c72fde1249bf4af
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-20 21:50:12 +0530

    adding header logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import "./header.css";
+
 const Header = () => {
   return (
-    <div className='max_width header'>
-     <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="Zomato-logo" className='header_logo' />
-    </div>
-  )
-}
+    <div className="max_width header">
+      <img
+        src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png"
+        alt="Zomato-logo"
+        className="header_logo"
+      />
 
-export default Header
+      <div className="header_left">
+        <div className="search_container">
+          <div className="location_wrapper">
+            <div className="location_icon">
+              <i className="fi fi-rr-marker"></i>
+              <div>Banglore</div>
+            </div>
+            <i className="fi fi-rr-caret-down absolute_center"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
