1780b2f2c8a7f8d85b3af29a0bd2caca6cebe61b
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-22 22:56:53 +0530

    all filters display on delivery tab

--- src/components/filters/Filter.jsx
+++ src/components/filters/Filter.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import "./filter.css"
-const Filter = () => {
+import React from "react";
+import "./filter.css";
+const Filter = ({ filterlist }) => {
   return (
-    <div>
-      this is filter
+    <div className="filters">
+      {filterlist &&
+        filterlist?.map((filter) => {
+          return (
+            <div id={filter?.id} className="filter_item">
+              <span>{filter?.icon && filter?.icon}</span>
+              <span className="filter_name">{filter?.title}</span>
+            </div>
+          );
+        })}
     </div>
-  )
-}
+  );
+};
 
-export default Filter
+export default Filter;
