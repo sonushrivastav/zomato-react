e69c7d377942d69b55c6092b2775dc3e900861db
Atomos Software Development Engineering <sonushrivastav1811@gmail.com>
2023-02-24 22:21:07 +0530

    fetching all details for card from api json

--- src/components/deliverytab/exploresection/explorecard/ExploreCard.jsx
+++ src/components/deliverytab/exploresection/explorecard/ExploreCard.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 import "./explorecard.css"
-const ExploreCard = () => {
+const ExploreCard = ({ restaurant }) => {
+    const name = restaurant?.info?.name ?? ""
+    const coverImg = restaurant?.info?.image?.url;
+    const deliveryTime = restaurant?.order?.deliveryTime;
+    const rating = restaurant?.info?.rating?.rating_text
+    const approxPrice = restaurant?.info?.cfo?.text
+    const offers = restaurant?.bulkOffers ?? []
+    const cuisines = restaurant?.info?.cuisine?.map((item)=>item?.name).slice(0,3)
+    const bottomContainers = restaurant?.bottomContainers;
+    const goldOff = restaurant?.gold?.text
+    const proOff  = offers.length > 1 ? offers[0].text : null
+    
   return (
     <div>
       explorecard section
